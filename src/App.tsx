06623ce9b12d15352c9327c8c992eff12d6bc048
raul rodriguez <raul.rod16124@gmail.com>
2024-11-30 18:55:45 +0000

    install dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './presentation/navigation/StackNavigator';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
 
 function App(): React.JSX.Element {
   return (
-    <NavigationContainer>
-      <StackNavigator />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
